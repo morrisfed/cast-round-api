d9801b6acd7362d8aee0497bac103ce85a559354
Daniel Watford <daniel@watfordconsulting.com>
2023-08-22 07:30:51 +0100

    Fix import of Role following refactor

--- src/api/interfaces/ProfileResponse.ts
+++ src/api/interfaces/ProfileResponse.ts
@@ -1,11 +1,11 @@
-import { Role } from "../../user/permissions";
+import { ModelRole } from "../../model/interfaces/model-roles";
 import { FrontEndFeatureFlags } from "../../utils/feature-flags";
 
 export default interface ProfileResponse {
   profile: {
     id: string;
     name: string;
-    roles: Role[];
+    roles: ModelRole[];
   };
   frontEndFeatureFlags: FrontEndFeatureFlags;
 }
