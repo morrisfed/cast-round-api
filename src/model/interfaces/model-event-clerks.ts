aea9cac6e69ab45c21ea92e3500dd917c8e98325
Daniel Watford <daniel@watfordconsulting.com>
2023-09-03 17:12:15 +0100

    Cleaned up model decoding process to use DataValuesFromModel codec to extract data values from Model objects before applying regular decoding.
    Introduced motion vote audit logging to track how votes are submitted over time and by whom.

--- src/model/interfaces/model-event-clerks.ts
+++ src/model/interfaces/model-event-clerks.ts
@@ -2,6 +2,7 @@ import * as t from "io-ts";
 
 import { ModelEvent } from "./model-events";
 import { ModelLinkUserDetails } from "./model-users";
+import { DataValuesFromFromModel } from "../db/interfaces/persisted";
 
 export const ModelEventClerk = t.strict({
   eventId: t.number,
@@ -9,13 +10,16 @@ export const ModelEventClerk = t.strict({
 });
 export type ModelEventClerk = t.TypeOf<typeof ModelEventClerk>;
 
-export const ModelEventClerkWithEventAndUserDetails = t.strict({
-  eventId: t.number,
-  clerkUserId: t.string,
+export const ModelEventClerkWithEventAndUserDetails =
+  DataValuesFromFromModel.pipe(
+    t.strict({
+      eventId: t.number,
+      clerkUserId: t.string,
 
-  event: ModelEvent,
-  clerkUser: ModelLinkUserDetails,
-});
+      event: ModelEvent,
+      clerkUser: ModelLinkUserDetails,
+    })
+  );
 export type ModelEventClerkWithEventAndUserDetails = t.TypeOf<
   typeof ModelEventClerkWithEventAndUserDetails
 >;
